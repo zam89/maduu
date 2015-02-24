be91aa8d0fc9cfa28c4fade11b5627d89559bb8e
M.Khairulazam <m.khairulazam@gmail.com>
2015-02-24 15:40:03 +0800

    Update clean.sh

--- opt/clean.sh
+++ opt/clean.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-#
 # Script to clean up log file in sensor
+#
 
 sudo /etc/init.d/p0f stop
 sudo /etc/init.d/dionaea stop
 sudo /etc/init.d/kippo stop
 sudo /etc/init.d/glastopf stop
+sleep 10s
 
 sudo rm -rf /var/p0f/p0f.log
 sudo rm -rf /opt/dionaea/var/log/*
@@ -14,6 +15,7 @@ sudo rm -rf /opt/dionaea/var/dionaea/bistreams/*
 sudo rm -rf /var/kippo/log/kippo.log*
 sudo rm -rf /opt/glaspot/log/*
 sudo cat /dev/null > /opt/bekasmadoo/running.log
+sleep 10s
 
 sudo /etc/init.d/p0f start
 sudo /etc/init.d/dionaea start
@@ -21,8 +23,6 @@ sudo /etc/init.d/kippo start
 sudo /etc/init.d/glastopf start
 
 sudo chown nobody:nogroup /tmp/p0f.sock
-sudo chown -R nobody:nogroup /opt/dionaea/var/dionaea
-sudo chown -R nobody:nogroup /opt/dionaea/var/log
+sudo chown -R nobody:nogroup /opt/dionaea/var/
 sudo chown -R kippo. /opt/kippo/
 sudo chown -R kippo. /var/kippo/
-sudo chown -R kippo. /var/kippo/log/
